9bfab60b03235057e2099dae3507a8ac0c04207b
tmueller <tim.mueller@clientis.com>
2018-07-31 08:56:44 +0200

    TeamFunc change

--- imports/ui/TeamFunc.js
+++ imports/ui/TeamFunc.js
@@ -7,15 +7,12 @@ export default class TeamFunc extends React.Component {
       render(){
         return(
           <div>
-            <h3>{this.props.team.name}</h3>
-          <div>
             <button onClick={() => {
               this.props.history.replace('/datelist/' + this.props.team._id)
             }} className="buttonColor">{this.props.team.name}</button>
             <button onClick={() => Meteor.call('teamFullRemove', this.props.team._id)} className="buttonColor">
               X
             </button>
-          </div>
           </div>
         );
       }
