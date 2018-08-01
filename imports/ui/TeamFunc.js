5367e4b8afdf34233349b677aee25274eb7949f9
stefanst <sstierstorfer@clientis.com>
2018-08-01 15:21:07 +0200

    test

--- imports/ui/TeamFunc.js
+++ imports/ui/TeamFunc.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Modal from 'react-modal';
 
 export default class TeamFunc extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showModalTeam: false,
 
+    }
+    this.handleOpenModalTeam = this.handleOpenModalTeam.bind(this);
+    this.handleCloseModalTeam = this.handleCloseModalTeam.bind(this);
+  }
+  componentWillMount() {
+    Modal.setAppElement('body');
+ }
+ handleOpenModalTeam () {
+   this.setState({ showModalTeam: true });
+  }
+  //schliesst popup für Termin hinzufuegen
+  handleCloseModalTeam () {
+    this.setState({ showModalTeam: false });
+  }
+  teamDelete(){
+    Meteor.call('teamFullRemove', this.props.team._id);
+    this.handleCloseModalTeam();
+  }
 
       render(){
         return(
@@ -10,9 +33,26 @@ export default class TeamFunc extends React.Component {
             <button className="buttonColor buttonTeamName" onClick={() => {
               this.props.history.replace('/datelist/' + this.props.team._id)
             }} >{this.props.team.name}</button>
-            <button className="buttonColor buttonTeamDel" onClick={() => Meteor.call('teamFullRemove', this.props.team._id)}>
+            <button className="buttonColor buttonTeamDel" onClick={this.handleOpenModalTeam}>
               X
             </button>
+
+
+            <Modal
+              appElement = {document.getElementById('body')}
+               isOpen={this.state.showModalTeam}
+               contentLabel="onRequestClose Example"
+               onRequestClose={this.handleCloseModalTeam}
+               shouldCloseOnOverlayClick={false}
+               className="boxed-view__box"
+               overlayClassName="boxed-view boxed-view--modal"
+            >
+              <p>Möchten sie wirklich diese Mannschaft löschen?</p>
+              <button  onClick={this.handleCloseModalTeam} className="buttonColor">Abbrechen</button>
+              <button  onClick={this.teamDelete.bind(this)} className="buttonColor">Löschen</button>
+
+            </Modal>
+
           </div>
         );
       }
